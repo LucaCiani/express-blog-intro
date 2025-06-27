271bf7cf2268f30a4550f347fa0fbb1bcd4fbf85
LucaCiani <lucaciani88@gmail.com>
2025-06-27 13:59:34 +0200

    scaffolding

---
+++ server.js
@@ -0,0 +1,13 @@
+// Creiamo il nostro blog personale e giorno dopo giorno lo potremo arricchire con nuove funzionalità sulla base di quello che impareremo.
+// Creiamo il progetto base con una rotta / che ritorna un testo semplice con scritto ”Server del mio blog”
+// Creiamo un array dove inserire una lista di almeno 5 post, per ognuno indicare titolo, contenuto, immagine e tags (tags è un array di stringhe)
+// Creiamo poi una rotta /api/posts che restituisca un oggetto json con la lista dei post.
+// Configuriamo gli asset statici sull’applicazione in modo che si possano visualizzare le immagini associate ad ogni post.
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server del mio blog in ascolto su http://localhost:${port}`);
+});
